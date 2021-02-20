f4e7e574ca6d382e1e85b2da88b3d13825e9cabd
Maria M <prov21_1@hotmail.com>
2021-02-19 20:34:23 -0700

    password criteria

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,27 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
+
+// declare function to choose criteria 
+// function generatePassword() {
+  // determine password length
+  var length = prompt('How long do you want the password to be? Pick a number between 8 and 128.')
+  if (length < 8 || length > 128 || (isNaN(length))) {
+  alert('Choose a number between 8 and 128.')
+  // return 
+  };
+
+  // determine type of characters
+  var lowercase = confirm('Do you want to include lowercase letters?');
+  var uppercase = confirm('Do you want to include uppercase letters?');
+  var numbers = confirm('Do you want to include numbers?');
+  var characters = confirm('Do you want to include special characters?');
+
+  if (lowercase  == false && uppercase  == false && numbers  == false && characters  == false) {
+    alert('You must choose at least one of the following options.')
+    // return /* or call function again */
+};
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +31,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+//};
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword)
