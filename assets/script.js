3bbd50de2d24fd5d3bfd64adf753ee7108dc4827
Maria M <prov21_1@hotmail.com>
2021-02-19 21:23:45 -0700

    arrays and password

--- assets/script.js
+++ assets/script.js
@@ -36,11 +36,48 @@ if (characters === true) {
   answerCount++;
 }
 
-function numOfCharFromArrays(num1, num2) {
-  return(Math.floor(num1/num2));
+// function numOfCharFromArrays(num1, num2) {
+//   return(Math.floor(num1/num2));
+// }
+
+// numOfCharFromArrays(length, answerCount);
+
+var picker = Math.floor(length/answerCount);
+console.log(picker);
+
+var lowercaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var uppercaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var charactersArr = ['!', '@', '$']
+
+var passwordContent = ""; 
+if (lowercase === true) {
+  // as long as index is less than picker, add a lowercase character to the string and repeat
+  // when the counter hits picker, the loop ends
+  for (i=0; i<picker; i++) {
+    // when you modify a string, you must store it again
+  passwordContent = passwordContent.concat(lowercaseArr[Math.floor(Math.random()*lowercaseArr.length)]);
+  }
 }
 
-numOfCharFromArrays(length, answerCount);
+if (uppercase === true) {
+  for (i=0; i<picker; i++) {
+  passwordContent = passwordContent.concat(uppercaseArr[Math.floor(Math.random()*uppercaseArr.length)]);
+  };
+}
+
+if (numbers === true) {
+  for (i=0; i<picker; i++) {
+    passwordContent = passwordContent.concat(numbersArr[Math.floor(Math.random()*numbersArr.length)]);
+  }
+}
+if (characters === true) {
+  for (i=0; i<picker; i++) {
+    passwordContent = passwordContent.concat(charactersArr[Math.floor(Math.random()*charactersArr.length)]);
+  }
+}
+  console.log(passwordContent);
+
 
 // Write password to the #password input
 function writePassword() {
